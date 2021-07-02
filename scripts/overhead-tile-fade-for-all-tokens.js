0392f71e4ad591ea0587d5ed1eb0ad1c2328103d
shortyverrett94 <cdverrett94@gmail.com>
2021-07-02 16:13:47 -0500

    add fade for all setting per tile
    added new setting to allow fade for all to be on a per tile basis. Default is to fade for all.

--- scripts/overhead-tile-fade-for-all-tokens.js
+++ scripts/overhead-tile-fade-for-all-tokens.js
@@ -1,12 +1,30 @@
 Hooks.on("ready", function() {
-     const updateOcclusionForAllTokens = function({sendSocket = true, tiles = canvas.foreground.tiles} = {}) {
-         for(tile of tiles) {
-            if(tile.data.occlusion.mode === CONST.TILE_OCCLUSION_MODES.FADE) {
-                tile.updateOcclusion(canvas.tokens.placeables);
+    //CONFIG.debug.hooks = true
+    const module = "overhead-tile-fade-for-all-tokens";
+    const updateOcclusionForAllTokens = function({sendSocket = true, tiles = canvas.foreground.tiles} = {}) {
+         tiles = tiles.filter(tile => tile.data.flags[module]?.activate !== false)
+         if(tiles.length > 0) {
+            for(tile of tiles) {
+                    if(tile.data.occlusion.mode === CONST.TILE_OCCLUSION_MODES.FADE) {
+                    tile.updateOcclusion(canvas.tokens.placeables);
+                }
             }
+            canvas.foreground.refresh();
+            if(sendSocket) game.socket.emit(`module.${module}`);
         }
-        canvas.foreground.refresh();
-        if(sendSocket) game.socket.emit('module.overhead-tile-fade-for-all-tokens');
+    }
+
+    const renderTileConfig = function(sheet, html) {
+        let tile = sheet.object;
+        let flags = tile.data.flags[module]
+
+        html.find('div.tab[data-tab="overhead"]').append(`
+            <h3>Overhead Tile Fade for All Tokens Settings</h3>
+            <div class="form-group">
+                <label>Fade Overhead Tiles Based For All?</label>
+                <input type="checkbox" name="flags.${module}.activate" ${(flags?.activate === false)? '':'checked="checked"'} />
+            </div>
+        `)
     }
 
     Hooks.on("sightRefresh", function() { updateOcclusionForAllTokens() });
@@ -15,5 +33,7 @@ Hooks.on("ready", function() {
             updateOcclusionForAllTokens({tiles: [tile._object], sendSocket: false});
         }
     });
-    game.socket.on('module.overhead-tile-fade-for-all-tokens', function() { updateOcclusionForAllTokens({sendSocket: false}) });
+    game.socket.on(`module.${module}`, function() { updateOcclusionForAllTokens({sendSocket: false}) });
+
+    Hooks.on('renderTileConfig', renderTileConfig)
 });
