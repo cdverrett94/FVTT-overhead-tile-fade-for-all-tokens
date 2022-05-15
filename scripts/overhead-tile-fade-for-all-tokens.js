831781861ba25a2f753761e22ef7ae6c6039da82
shortyverrett94 <cdverrett94@gmail.com>
2022-05-15 15:24:02 -0500

    refactor to remove sight refresh from occlusion update

--- scripts/overhead-tile-fade-for-all-tokens.js
+++ scripts/overhead-tile-fade-for-all-tokens.js
@@ -1,68 +1,62 @@
 const MODULE_NAME = 'overhead-tile-fade-for-all-tokens';
 
-globalThis.otffat = {
-  counter: 0,
-  anyRadialTiles: false,
-  descriptions: {
-    OFF: 'Provides the Foundry Default functionality where an overhead tile is only faded for the token that is under the tile.',
-    BY_VISIBILITY: 'This mode will fade an overhead tile as long as the token has visibility on the token under the tile.',
-  },
-};
+const includeRgx = new RegExp(`/modules/${MODULE_NAME}/scripts/${MODULE_NAME}.js`);
+CONFIG.compatibility.includePatterns.push(includeRgx);
 
-const updateOcclusion = function (wrapped, tokens) {
-  const numberOfTiles = canvas.foreground.tiles.length;
-  globalThis.otffat.counter = globalThis.otffat.counter ?? 0;
+function fadeByTokenVisibility(tile) {
+  return tile.document.occlusion.mode !== CONST.TILE_OCCLUSION_MODES.NONE && tile.document.occlusion.mode !== CONST.TILE_OCCLUSION_MODES.ROOF && tile.document.flags[MODULE_NAME]?.mode !== 'OFF';
+}
 
-  //refresh vision to get accurate token.visible data. check put in place to prevent sight refresh more than once per ForegroundLayer#updateOcclusion
-  if (globalThis.otffat.counter === 0) {
-    canvas.effects.visibility.initializeSources();
-    canvas.effects.visibility.refresh();
-  }
-
-  if (this.document.flags[MODULE_NAME]?.mode !== 'OFF') {
+function updateOcclusion(wrapped, tokens) {
+  if (fadeByTokenVisibility(this)) {
     tokens = canvas.tokens.placeables.filter((token) => token.visible);
   }
 
-  if (globalThis.otffat.anyRadialTiles) canvas.foreground._drawOcclusionShapes(tokens);
+  wrapped(tokens);
+}
 
-  globalThis.otffat.counter = globalThis.otffat.counter >= numberOfTiles - 1 ? 0 : globalThis.otffat.counter + 1;
+function drawOcclusionShapes(wrapped, tokens) {
+  if (canvas.foreground.tiles.some(fadeByTokenVisibility)) tokens = canvas.tokens.placeables.filter((token) => token.visible);
   return wrapped(tokens);
-};
+}
 
-// update if there are any radial tiles that are affected by this module. Prevents unecessary calls to ForegroundLayer#_drawOcclusionShapes
-const updateAnyRadialTiles = function () {
-  globalThis.otffat.anyRadialTiles = !!canvas.foreground.tiles.find((tile) => tile.document.flags[MODULE_NAME]?.mode !== 'OFF' && tile.document.occlusion.mode === CONST.TILE_OCCLUSION_MODES.RADIAL);
-};
+Hooks.on('sightRefresh', () => {
+  // if there's a overhead tile that depends on token visibility, schedule a foreground refesh to recalculate occlusion states
+  // otherwise draw all visible tokens in the radial occlusion mask
+  if (canvas.foreground.tiles.some(fadeByTokenVisibility)) {
+    canvas.perception.schedule({ foreground: { refresh: true } });
+  } else {
+    canvas.foreground._drawOcclusionShapes(canvas.tokens.controlled);
+  }
+});
 
-// trigger occlusion update when a tile is updated so new OTFFAT settings are taken into account immediately
-const triggerOcclusionUpdate = function () {
-  canvas.foreground.updateOcclusion();
-};
-
-// add off/on setting per tile
+// add off/on settings per tile
 const renderTileConfig = function (sheet, html) {
-  let tile = sheet.document;
+  let tile = sheet.object;
   let flags = tile.flags[MODULE_NAME];
 
   let mode = flags?.mode ?? 'BY_VISIBILITY';
 
+  const descriptions = {
+    OFF: 'Provides the Foundry Default functionality where an overhead tile is only faded for the token that is under the tile.',
+    BY_VISIBILITY: 'This mode will fade an overhead tile as long as the token has visibility on the token under the tile.',
+  };
+
   html.find('div.tab[data-tab="overhead"]').append(`
-    <h2>Overhead Tile Fade for All Tokens Settings</h2>
     <div class="form-group">
-      <label>Mode:</label>
+      <label>Occlusion By Visibility</label>
       <select id="otffat_mode_select_${tile.id}" name="flags.${MODULE_NAME}.mode">
         <option value="OFF" ${mode === 'OFF' ? 'selected' : ''}>Foundry Default (Off)</option>
         <option value="BY_VISIBILITY" ${mode === 'BY_VISIBILITY' ? 'selected' : ''}>By Token Visibility</option>
       </select>
     </div>
-    <p class="notes" id="otffat_mode_descriptions_${tile.id}">${globalThis.otffat.descriptions[mode]}</p>
-    <h2></h2>
+    <p class="notes" id="otffat_mode_descriptions_${tile.id}">${descriptions[mode]} Does not affect Roof tiles.</p>
   `);
 
   $(`#otffat_mode_select_${tile.id}`).change(function () {
     let new_mode = $(this).val();
     let id = $(this).attr('id').split('otffat_mode_select_')[1];
-    $(`#otffat_mode_descriptions_${id}`).text(globalThis.otffat.descriptions[new_mode]);
+    $(`#otffat_mode_descriptions_${id}`).text(descriptions[new_mode]);
   });
 
   sheet.setPosition({ height: 'auto' });
@@ -70,12 +64,13 @@ const renderTileConfig = function (sheet, html) {
 
 Hooks.on('libWrapper.Ready', function () {
   libWrapper.register(MODULE_NAME, 'Tile.prototype.updateOcclusion', updateOcclusion, 'WRAPPER');
+  libWrapper.register(MODULE_NAME, 'ForegroundLayer.prototype._drawOcclusionShapes', drawOcclusionShapes, 'WRAPPER');
 });
 
 Hooks.on('renderTileConfig', renderTileConfig);
-
-Hooks.on('canvasReady', updateAnyRadialTiles);
-Hooks.on('createTile', updateAnyRadialTiles);
-Hooks.on('updateTile', updateAnyRadialTiles);
-
-Hooks.on('updateTile', triggerOcclusionUpdate);
+Hooks.on('updateTile', (document, change) => {
+  // trigger occlusion update when a tile is updated so new OTFFAT settings are taken into account immediately
+  if (change.flags?.[MODULE_NAME]?.mode !== undefined) {
+    canvas.perception.schedule({ foreground: { refresh: true } });
+  }
+});
